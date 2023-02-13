e95a6a2b0547639146e1308283f51b66ea30a272
MarmoW <115847012+MarmoW@users.noreply.github.com>
2023-02-12 21:49:43 -0300

    fixes

--- src/controllers/CustomerController.js
+++ src/controllers/CustomerController.js
@@ -1,12 +1,17 @@
 import { CustomerSchema } from "../schema/CustomerSchema.js";
 import {db} from '../database/database.connection.js';
+import { response } from "express";
 
 export async function SignUp(req, res){
     const {name, phone, cpf, birthday} = req.body;
 
     try{
         const CheckCpf = await db.query("SELECT * FROM customers WHERE cpf =$1", [cpf]);
+        if(CheckCpf.length > 0) return res.status(409).send();
 
+        await db.query("INSERT INTO customers (name, phone, cpf, birthday) WHERE ($1,$2,$3,$4)", [name, phone, cpf, birthday])
+
+        res.status(201).send();
         
     }catch{
 
--- src/controllers/GameController.js
+++ src/controllers/GameController.js
@@ -25,7 +25,7 @@ export async function InsertGames(req, res){
 
         await db.query("INSERT INTO games (name, image, stockTotal, pricePerDay) VALUES ($1, $2, $3, $4)",[name, image, stockTotal, pricePerDay]);
 
-        res.send().status(201);
+        res.status(201).send();
 
     }catch(err){
         res.send(err.message);
