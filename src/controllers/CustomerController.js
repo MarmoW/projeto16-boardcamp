03066007684c8f74b240444ae7c97e1db353ebbf
MarmoW <115847012+MarmoW@users.noreply.github.com>
2023-02-13 10:45:39 -0300

    fixes

--- src/controllers/CustomerController.js
+++ src/controllers/CustomerController.js
@@ -24,12 +24,13 @@ export async function UpdateUser(req, res){
     const {name, phone, cpf, birthday} = req.body;
 
     try{
-        const checkCpf = await db.query(`SELECT * FROM customers WHERE cpf=$1`,[cpf])
+        const checkCpf = await db.query(`SELECT * FROM customers WHERE cpf=$1`,[cpf]);
+        const checkId = await db.query(`SELECT * FROM customers WHERE id=$1`,[id])
 
         if(checkCpf.rows.length > 0 && checkCpf.rows[0].id != id) return res.sendStatus(409);
-
-        if(checkCpf.rows.length < 1) return res.sendStatus(409);
-                  
+        
+        if(checkId.rows.length < 1) return res.sendStatus(409);
+        
         await db.query(`UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5`, [name, phone, cpf, birthday, id]);
         
         res.sendStatus(200);
